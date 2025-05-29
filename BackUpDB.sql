1a83b682e5a07908faf002bcaf05ef1eafc5d401
rahmaAlmamari <alr816125@gmail.com>
2025-05-29 11:19:11 +0400

    Start Back up database test

---
+++ BackUpDB.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE TrainingDB; 
+GO 
+USE TrainingDB; 
+GO 
+CREATE TABLE Students ( 
+StudentID INT PRIMARY KEY, 
+FullName NVARCHAR(100), 
+EnrollmentDate DATE 
+); 
+INSERT INTO Students VALUES  
+(1, 'Sara Ali', '2023-09-01'), 
+(2, 'Mohammed Nasser', '2023-10-15'); 
+
+SELECT * FROM Students;
